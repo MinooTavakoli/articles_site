ce3037c9b6506c146356ea5f9fb321273b3ba735
minatavakoli <minatavakoli74@gmail.com>
2022-06-28 00:30:24 +0430

    add title component

---
+++ src/home/sections/title/title.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./title.css";
+
+function Title() {
+  return (
+    <div>
+      <div>GET INFORMED</div>
+      <div>GET INSPIRED</div>
+      <div>Stories Curated For You</div>
+    </div>
+  );
+}
+
+export default Title;
