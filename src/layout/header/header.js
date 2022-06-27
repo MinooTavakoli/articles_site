435ba65fa4d720a14eb80b7b63f5c315921055f6
minatavakoli <minatavakoli74@gmail.com>
2022-06-28 00:30:57 +0430

    add header component

---
+++ src/layout/header/header.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./header.css";
+import flipboard_logo from "../../assets/images/Flipboard_logo.png";
+import Button from "../../components/buttons/button";
+
+function Header() {
+  return (
+    <div className="header-component">
+      <div className="header-wrapper">
+        <div className="flipboard-logo-wrapper">
+          <img
+            src={flipboard_logo}
+            alt="flipboard_logo"
+            className="flipboard-logo-image"
+          />
+        </div>
+        <div className="operation-wrapper">
+          <div className="newsletters-title-section">Newsletters</div>
+          <div>
+            <input
+              className="operation-input-search"
+              type="text"
+              placeholder="Search Flipboard"
+            />
+            <div className="operation-button-wrapper">
+              <Button label="Open in app" type="outline" />
+              <Button label="Sign up" type="danger" />
+              <Button label="Log in" type="none" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
