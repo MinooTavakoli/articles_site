0ffc814103dcf1e0e55bea715f0df92fee668a97
mi.tavakoli <mi.tavakoli@asiatech.ir>
2022-06-28 13:14:45 +0430

    change header style and add footer component

--- src/layout/footer/footer.js
+++ src/layout/footer/footer.js
@@ -1,8 +1,28 @@
 import React from "react";
-import'./footer.css'
+import './footer.css'
 
 function Footer() {
-  return <div>footer</div>;
+  return (
+    <div className="footer-component">
+      <div className="footer-wrapper">
+        <h6>Log in</h6>
+        <h6>Newsletters</h6>
+        <h6>About Us</h6>
+        <h6>Blog</h6>
+        <h6>Tools</h6>
+        <h6>Publishers</h6>
+        <h6>Help</h6>
+        <h6>Terms</h6>
+        <h6>Privacy Policy</h6>
+        <h6>Data Protection</h6>
+        <h6>Discover</h6>
+        <h6>Sitemap</h6>
+      </div>
+      <div className="description-footer">
+        © 2022 made with ❤️ made by <span className="description-name">Minoo Tavakoli</span> for a better web.
+      </div>
+    </div>
+  )
 }
 
 export default Footer;
