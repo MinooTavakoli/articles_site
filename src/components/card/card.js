10b645dbfad8f50103d2f4be4854c005d1409c50
mi.tavakoli <mi.tavakoli@asiatech.ir>
2022-06-28 13:17:19 +0430

    add card component

---
+++ src/components/card/card.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './card.css'
+
+function Card({ title = "", logo = undefined, logoCenter = undefined, publishTime = "", description = "", likeIcon = undefined, uploadIcon = undefined, plusIcon = undefined, likesCount = "" }) {
+    return (
+        <div className="card-component">
+            <div className="card-wrapper">
+                <div className="card-head-section">
+                    {logo && <img src={logo} alt="logo_news" className="card-logo-news" />}
+                    <div className="card-title-news">{title}</div>
+                </div>
+                <div className="card-center-section">
+                    {logoCenter && <img src={logoCenter} alt="logo_news" className="card-logo-news-center" />}
+                </div>
+                <div className="card-publish-time">{publishTime}</div>
+                <p className="description-news">{description}</p>
+                <div className="card-footer-section-wrapper">
+                    <div className="card-footer-section">
+                        {likeIcon && <div className="footer-like-icon"><img src={likeIcon} alt="like_icon" className="footer-icon" /><span>{likesCount}</span></div>}
+                        <div className="footer-icons-wrapper">
+                            {uploadIcon && <div><img src={uploadIcon} alt="upload_icon" className="footer-icon" /></div>}
+                            {plusIcon && <div><img src={plusIcon} alt="plus_icon" className="footer-icon" /></div>}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
