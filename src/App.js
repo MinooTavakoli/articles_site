d417857a4f01c96358407e2876a8e5be0b3e271c
minatavakoli <minatavakoli74@gmail.com>
2022-06-29 01:38:55 +0430

    add postId route to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import logo from "./logo.ico";
-import Home from "./home/home";
+import React from "react";
 import { useRoutes } from "react-router-dom";
+import Home from "./home/home";
+import PostItem from "./home/postItem/postItem";
 import "./App.css";
 
 export default function App() {
-  let element = useRoutes([{ path: "/", element: <Home /> }]);
+  let element = useRoutes([
+    { path: "/", element: <Home /> },
+    { path: "postItem/:postId", element: <PostItem /> },
+  ]);
   return element;
 }
