da8e559b3a18f43d8d0268b8bea79a8f9e6c585c
MinooTavakoli <minootavakoli28@gmail.com>
2022-06-30 14:53:26 +0430

    add routes to app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { useRoutes } from "react-router-dom";
 import Home from "./home/home";
 import PostItem from "./home/postItem/postItem";
 import "./App.css";
 
 export default function App() {
-    let element = useRoutes([
-        { path: "/", element: <Home /> },
-        { path: "postItem/:postId", element: <PostItem /> },
-    ]);
-    return element;
+  const [searchResultPage, setSearchResultPage] = useState();
+  const [search, setSearch] = useState("");
+
+  let element = useRoutes([
+    {
+      path: "/",
+      element: (
+        <Home
+          searchResultPage={searchResultPage}
+          setSearchResultPage={setSearchResultPage}
+          search={search}
+          setSearch={setSearch}
+        />
+      ),
+    },
+    {
+      path: "postItem/:postId",
+      element: <PostItem searchResultPage={searchResultPage} />,
+    },
+  ]);
+  return element;
 }
